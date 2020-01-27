29efb8babcb92fca585e0b5744ea1f1fc43400f8
Ama <ama6nen@gmail.com>
2020-01-27 23:18:00 +0200

    commenting, cleanup

--- gameoflife/form.cs
+++ gameoflife/form.cs
@@ -18,39 +18,41 @@ namespace gameoflife
             InitializeComponent();
         }
 
-        float MouseX = 0.0f;
-        float MouseY = 0.0f;
-        int RefreshRate = 144; //use 60 if your screen is 60hz
+        private void form_Load(object sender, EventArgs e)
+        => Size = new Size(StartPadding + (CellCount - 2) * CellSize, StartPadding + CellCount * CellSize + 3);
+
+        readonly int RefreshRate = 144; //use 60 if your screen is 60hz
+        readonly int StartPadding = 40; //dont change this
+
+        //Mouse information
+        Point MousePos = new Point(0, 0);
         bool ClickedMouse = false;
 
+        //Cell information
         int CellCount = 60;
         int CellSize = 10;
-        int StartPadding = 40;
-        private void form_Load(object sender, EventArgs e)
-        {
-            Size = new Size(StartPadding + (CellCount - 2) * CellSize, StartPadding + CellCount * CellSize + 3);
-        }
-
-
         List<Point> AliveCells = new List<Point>();
+
+
         protected override async void OnPaint(PaintEventArgs e)
         {
             var StartTime = DateTime.Now;
             int MaxNumber = StartPadding / CellSize;
-            for (int i = 0; i < CellCount + 1; i++)
+            for (int i = 0; i < CellCount + 1; i++) //draw the grid
             {
                 e.Graphics.DrawLine(Pens.Black, i * CellSize, StartPadding, i * CellSize, CellCount * CellSize);
-                if (i >= MaxNumber)
+                if (i >= MaxNumber) //we dont want to draw for the first StartPadding pixels due to it containing buttons and such
                     e.Graphics.DrawLine(Pens.Black, 0, i * CellSize, CellCount * CellSize, i * CellSize);
             }
 
-            int CellX = (int)(MouseX - StartPadding) / CellSize;
-            int CellY = (int)(MouseY - StartPadding) / CellSize;
+            int CellX = (MousePos.X - StartPadding) / CellSize;
+            int CellY = (MousePos.Y - StartPadding) / CellSize;
 
-            foreach (var point in AliveCells)  
+            foreach (var point in AliveCells) //draw filled rectangles of alive cells
                 e.Graphics.FillRectangle(Brushes.Black, new RectangleF(point.X * CellSize + StartPadding, point.Y * CellSize + StartPadding, CellSize, CellSize));
 
 
+            //check if hypothetical cell is within actual grid and not for example at the buttons
             if (CellX >= -MaxNumber && CellY >= 0 && CellX < CellCount - MaxNumber && CellY < CellCount - MaxNumber)
             {
                 if (ClickedMouse)
@@ -62,21 +64,19 @@ namespace gameoflife
                         AliveCells.Add(point);
                     ClickedMouse = false;
                 }
+                //Show mouse hover as gray
                 e.Graphics.FillRectangle(Brushes.Gray, new RectangleF(CellX * CellSize + StartPadding, CellY * CellSize + StartPadding, CellSize, CellSize));
-
             }
 
 
+            //refresh accordingly
             await Task.Delay(Math.Max(1000 / RefreshRate - (int)(DateTime.Now - StartTime).TotalMilliseconds, 0));
             this.Invalidate();
             base.OnPaint(e);
         }
 
         private void form_MouseMove(object sender, MouseEventArgs e)
-        {
-            MouseX = (float)e.X;
-            MouseY = (float)e.Y;
-        }
+        => MousePos = new Point(e.X, e.Y);
 
         private void CellNumeric_ValueChanged(object sender, EventArgs e)
         {
@@ -85,8 +85,7 @@ namespace gameoflife
         }
 
         private void form_MouseUp(object sender, MouseEventArgs e)
-        {
-            ClickedMouse = true;
-        }
+        => ClickedMouse = true;
+
     }
 }
