f7d360571c1f033b05402aa689ebdaf8ca5001a0
Ama <ama6nen@gmail.com>
2020-01-27 23:11:19 +0200

    Basic cell adding logic

--- gameoflife/form.Designer.cs
+++ gameoflife/form.Designer.cs
@@ -28,14 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.CellNumeric = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.CellNumeric)).BeginInit();
             this.SuspendLayout();
+            // 
+            // CellNumeric
+            // 
+            this.CellNumeric.Location = new System.Drawing.Point(264, 7);
+            this.CellNumeric.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.CellNumeric.Maximum = new decimal(new int[] {
+            200,
+            0,
+            0,
+            0});
+            this.CellNumeric.Minimum = new decimal(new int[] {
+            40,
+            0,
+            0,
+            0});
+            this.CellNumeric.Name = "CellNumeric";
+            this.CellNumeric.Size = new System.Drawing.Size(62, 25);
+            this.CellNumeric.TabIndex = 0;
+            this.CellNumeric.Value = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.CellNumeric.ValueChanged += new System.EventHandler(this.CellNumeric_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(332, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Cell Count";
             // 
             // form
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(763, 473);
+            this.ClientSize = new System.Drawing.Size(727, 440);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CellNumeric);
             this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "form";
@@ -43,11 +83,17 @@
             this.Text = "Game of Life";
             this.Load += new System.EventHandler(this.form_Load);
             this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.form_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.form_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this.CellNumeric)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.NumericUpDown CellNumeric;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- gameoflife/form.cs
+++ gameoflife/form.cs
@@ -21,27 +21,51 @@ namespace gameoflife
         float MouseX = 0.0f;
         float MouseY = 0.0f;
         int RefreshRate = 144; //use 60 if your screen is 60hz
+        bool ClickedMouse = false;
 
         int CellCount = 60;
         int CellSize = 10;
-        int StartPadding = 30;
+        int StartPadding = 40;
         private void form_Load(object sender, EventArgs e)
         {
-
+            Size = new Size(StartPadding + (CellCount - 2) * CellSize, StartPadding + CellCount * CellSize + 3);
         }
 
 
+        List<Point> AliveCells = new List<Point>();
         protected override async void OnPaint(PaintEventArgs e)
         {
             var StartTime = DateTime.Now;
-
-            for (int i = StartPadding / CellSize; i < CellCount; i++)
+            int MaxNumber = StartPadding / CellSize;
+            for (int i = 0; i < CellCount + 1; i++)
             {
                 e.Graphics.DrawLine(Pens.Black, i * CellSize, StartPadding, i * CellSize, CellCount * CellSize);
-                e.Graphics.DrawLine(Pens.Black, StartPadding, i * CellSize, CellCount * CellSize, i * CellSize);
+                if (i >= MaxNumber)
+                    e.Graphics.DrawLine(Pens.Black, 0, i * CellSize, CellCount * CellSize, i * CellSize);
             }
 
-            e.Graphics.FillRectangle(Brushes.Red, new RectangleF(MouseX - 2.5f, MouseY - 2.5f, 5, 5));
+            int CellX = (int)(MouseX - StartPadding) / CellSize;
+            int CellY = (int)(MouseY - StartPadding) / CellSize;
+
+            foreach (var point in AliveCells)  
+                e.Graphics.FillRectangle(Brushes.Black, new RectangleF(point.X * CellSize + StartPadding, point.Y * CellSize + StartPadding, CellSize, CellSize));
+
+
+            if (CellX >= -MaxNumber && CellY >= 0 && CellX < CellCount - MaxNumber && CellY < CellCount - MaxNumber)
+            {
+                if (ClickedMouse)
+                {
+                    var point = new Point(CellX, CellY);
+                    if (AliveCells.Contains(point))
+                        AliveCells.Remove(point);
+                    else
+                        AliveCells.Add(point);
+                    ClickedMouse = false;
+                }
+                e.Graphics.FillRectangle(Brushes.Gray, new RectangleF(CellX * CellSize + StartPadding, CellY * CellSize + StartPadding, CellSize, CellSize));
+
+            }
+
 
             await Task.Delay(Math.Max(1000 / RefreshRate - (int)(DateTime.Now - StartTime).TotalMilliseconds, 0));
             this.Invalidate();
@@ -52,6 +76,17 @@ namespace gameoflife
         {
             MouseX = (float)e.X;
             MouseY = (float)e.Y;
+        }
+
+        private void CellNumeric_ValueChanged(object sender, EventArgs e)
+        {
+            CellCount = (int)CellNumeric.Value;
+            Size = new Size(StartPadding + (CellCount - 2) * CellSize, StartPadding + CellCount * CellSize + 3);
+        }
+
+        private void form_MouseUp(object sender, MouseEventArgs e)
+        {
+            ClickedMouse = true;
         }
     }
 }
