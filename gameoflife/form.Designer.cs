8921201804bbb4bbf7b9bf6d8e115ff1e7c57308
Ama <ama6nen@gmail.com>
2020-01-28 17:21:29 +0200

    Finish the program

--- gameoflife/form.Designer.cs
+++ gameoflife/form.Designer.cs
@@ -28,14 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.CellNumeric = new System.Windows.Forms.NumericUpDown();
             this.label1 = new System.Windows.Forms.Label();
+            this.StepButton = new System.Windows.Forms.Button();
+            this.GOFTimer = new System.Windows.Forms.Timer(this.components);
+            this.ToggleGOF = new System.Windows.Forms.Button();
+            this.Delay = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.SaveConfiguration = new System.Windows.Forms.Button();
+            this.LoadConfiguration = new System.Windows.Forms.Button();
+            this.SaveDialog = new System.Windows.Forms.SaveFileDialog();
+            this.OpenDialog = new System.Windows.Forms.OpenFileDialog();
             ((System.ComponentModel.ISupportInitialize)(this.CellNumeric)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Delay)).BeginInit();
             this.SuspendLayout();
             // 
             // CellNumeric
             // 
-            this.CellNumeric.Location = new System.Drawing.Point(264, 7);
+            this.CellNumeric.Location = new System.Drawing.Point(468, 7);
             this.CellNumeric.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.CellNumeric.Maximum = new decimal(new int[] {
             200,
@@ -43,7 +55,7 @@
             0,
             0});
             this.CellNumeric.Minimum = new decimal(new int[] {
-            40,
+            60,
             0,
             0,
             0});
@@ -60,17 +72,121 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(332, 9);
+            this.label1.Location = new System.Drawing.Point(536, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(67, 17);
             this.label1.TabIndex = 1;
             this.label1.Text = "Cell Count";
             // 
+            // StepButton
+            // 
+            this.StepButton.Location = new System.Drawing.Point(68, 5);
+            this.StepButton.Name = "StepButton";
+            this.StepButton.Size = new System.Drawing.Size(50, 25);
+            this.StepButton.TabIndex = 2;
+            this.StepButton.Text = "Step";
+            this.StepButton.UseVisualStyleBackColor = true;
+            this.StepButton.Click += new System.EventHandler(this.StepButton_Click);
+            // 
+            // GOFTimer
+            // 
+            this.GOFTimer.Tick += new System.EventHandler(this.GOFTimer_Tick);
+            // 
+            // ToggleGOF
+            // 
+            this.ToggleGOF.Location = new System.Drawing.Point(12, 5);
+            this.ToggleGOF.Name = "ToggleGOF";
+            this.ToggleGOF.Size = new System.Drawing.Size(50, 25);
+            this.ToggleGOF.TabIndex = 3;
+            this.ToggleGOF.Text = "Start";
+            this.ToggleGOF.UseVisualStyleBackColor = true;
+            this.ToggleGOF.Click += new System.EventHandler(this.ToggleGOF_Click);
+            // 
+            // Delay
+            // 
+            this.Delay.Location = new System.Drawing.Point(345, 7);
+            this.Delay.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Delay.Maximum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.Delay.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.Delay.Name = "Delay";
+            this.Delay.Size = new System.Drawing.Size(62, 25);
+            this.Delay.TabIndex = 5;
+            this.Delay.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.Delay.ValueChanged += new System.EventHandler(this.Delay_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(413, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Delay";
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(124, 5);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(48, 25);
+            this.ClearButton.TabIndex = 7;
+            this.ClearButton.Text = "Clear";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // SaveConfiguration
+            // 
+            this.SaveConfiguration.Location = new System.Drawing.Point(209, 5);
+            this.SaveConfiguration.Name = "SaveConfiguration";
+            this.SaveConfiguration.Size = new System.Drawing.Size(62, 25);
+            this.SaveConfiguration.TabIndex = 8;
+            this.SaveConfiguration.Text = "Save";
+            this.SaveConfiguration.UseVisualStyleBackColor = true;
+            this.SaveConfiguration.Click += new System.EventHandler(this.SaveConfiguration_Click);
+            // 
+            // LoadConfiguration
+            // 
+            this.LoadConfiguration.Location = new System.Drawing.Point(277, 5);
+            this.LoadConfiguration.Name = "LoadConfiguration";
+            this.LoadConfiguration.Size = new System.Drawing.Size(62, 25);
+            this.LoadConfiguration.TabIndex = 9;
+            this.LoadConfiguration.Text = "Load";
+            this.LoadConfiguration.UseVisualStyleBackColor = true;
+            this.LoadConfiguration.Click += new System.EventHandler(this.LoadConfiguration_Click);
+            // 
+            // SaveDialog
+            // 
+            this.SaveDialog.Filter = "Game of Life files|*.gof";
+            this.SaveDialog.Title = "Save Configuration";
+            // 
+            // OpenDialog
+            // 
+            this.OpenDialog.Filter = "Game of Life files|*.gof";
+            this.OpenDialog.Title = "Load configuration";
+            // 
             // form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(727, 440);
+            this.ClientSize = new System.Drawing.Size(605, 444);
+            this.Controls.Add(this.LoadConfiguration);
+            this.Controls.Add(this.SaveConfiguration);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Delay);
+            this.Controls.Add(this.ToggleGOF);
+            this.Controls.Add(this.StepButton);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.CellNumeric);
             this.DoubleBuffered = true;
@@ -85,6 +201,7 @@
             this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.form_MouseMove);
             this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.form_MouseUp);
             ((System.ComponentModel.ISupportInitialize)(this.CellNumeric)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Delay)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -94,6 +211,16 @@
 
         private System.Windows.Forms.NumericUpDown CellNumeric;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button StepButton;
+        private System.Windows.Forms.Timer GOFTimer;
+        private System.Windows.Forms.Button ToggleGOF;
+        private System.Windows.Forms.NumericUpDown Delay;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.Button SaveConfiguration;
+        private System.Windows.Forms.Button LoadConfiguration;
+        private System.Windows.Forms.SaveFileDialog SaveDialog;
+        private System.Windows.Forms.OpenFileDialog OpenDialog;
     }
 }
 
--- gameoflife/form.cs
+++ gameoflife/form.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -45,15 +42,14 @@ namespace gameoflife
                     e.Graphics.DrawLine(Pens.Black, 0, i * CellSize, CellCount * CellSize, i * CellSize);
             }
 
-            int CellX = (MousePos.X - StartPadding) / CellSize;
+            int CellX = MousePos.X / CellSize;
             int CellY = (MousePos.Y - StartPadding) / CellSize;
 
             foreach (var point in AliveCells) //draw filled rectangles of alive cells
-                e.Graphics.FillRectangle(Brushes.Black, new RectangleF(point.X * CellSize + StartPadding, point.Y * CellSize + StartPadding, CellSize, CellSize));
-
+                e.Graphics.FillRectangle(Brushes.Black, new RectangleF(point.X * CellSize, point.Y * CellSize + StartPadding, CellSize, CellSize));
 
             //check if hypothetical cell is within actual grid and not for example at the buttons
-            if (CellX >= -MaxNumber && CellY >= 0 && CellX < CellCount - MaxNumber && CellY < CellCount - MaxNumber)
+            if (CellX >= -MaxNumber && CellY >= 0 && CellX < CellCount && CellY < CellCount - MaxNumber)
             {
                 if (ClickedMouse)
                 {
@@ -65,7 +61,7 @@ namespace gameoflife
                     ClickedMouse = false;
                 }
                 //Show mouse hover as gray
-                e.Graphics.FillRectangle(Brushes.Gray, new RectangleF(CellX * CellSize + StartPadding, CellY * CellSize + StartPadding, CellSize, CellSize));
+                e.Graphics.FillRectangle(Brushes.Gray, new RectangleF(CellX * CellSize, CellY * CellSize + StartPadding, CellSize, CellSize));
             }
 
 
@@ -87,5 +83,113 @@ namespace gameoflife
         private void form_MouseUp(object sender, MouseEventArgs e)
         => ClickedMouse = true;
 
+        //inefficient algorithm, but it will do for small games with no notable performance loss
+        int GetAliveNeighborCount(int x, int y)
+        {
+            int count = 0;
+
+            for (int a = -1; a <= 1; a++) //basically check if any of the 8 close neighbors are alive
+            {
+                for (int b = -1; b <= 1; b++)
+                {
+                    if (a == 0 && b == 0)
+                        continue;
+
+                    if (AliveCells.Contains(new Point(x + a, y + b)))
+                        count++;
+                }
+            }
+            return count;
+        }
+
+        int Generation = 0;
+        void Step()
+        {
+            int MaxNumber = StartPadding / CellSize;
+            var NewAliveCells = new List<Point>();
+            for (int x = 0; x < CellCount; x++)
+            {
+                for (int y = 0; y < CellCount - MaxNumber; y++)
+                {
+                    int neighbors = GetAliveNeighborCount(x, y);
+                    if (AliveCells.Contains(new Point(x, y))) //basically check if cell is alive
+                    {
+                        //dont let overpopulated or underpopulated cells go into next gneeration
+                        if (neighbors == 2 || neighbors == 3)
+                            NewAliveCells.Add(new Point(x, y));
+                    }
+                    else if (neighbors == 3) //if a cell is dead and has 3 neighbors, it is "born"
+                        NewAliveCells.Add(new Point(x, y));
+                }
+            }
+            AliveCells = NewAliveCells;
+            Generation++;
+            this.Text = "Game of Life - Generation: " + Generation;
+        }
+        private void StepButton_Click(object sender, EventArgs e)
+        => Step();
+
+        private void GOFTimer_Tick(object sender, EventArgs e)
+        => Step();
+
+        private void ToggleGOF_Click(object sender, EventArgs e)
+        {
+            if (ToggleGOF.Text == "Start")
+            {
+                GOFTimer.Start();
+                ToggleGOF.Text = "Stop";
+            }
+            else
+            {
+                GOFTimer.Stop();
+                ToggleGOF.Text = "Start";
+            }
+        }
+
+        private void Delay_ValueChanged(object sender, EventArgs e)
+        => GOFTimer.Interval = (int)Delay.Value;
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            AliveCells.Clear();
+            Generation = 0;
+        }
+
+        private void SaveConfiguration_Click(object sender, EventArgs e)
+        {
+            if(SaveDialog.ShowDialog() == DialogResult.OK)
+            {
+                var stream = new FileStream(SaveDialog.FileName, FileMode.OpenOrCreate);
+                var writer = new BinaryWriter(stream);
+
+                foreach(var point in AliveCells)
+                {
+                    writer.Write(point.X);
+                    writer.Write(point.Y);
+                }
+                writer.Close();
+                stream.Close();
+            }
+        }
+
+        private void LoadConfiguration_Click(object sender, EventArgs e)
+        {
+            if (OpenDialog.ShowDialog() == DialogResult.OK)
+            {
+                AliveCells.Clear();
+                var stream = new FileStream(OpenDialog.FileName, FileMode.Open);
+                var reader = new BinaryReader(stream);
+
+                while (stream.Position < stream.Length && stream.CanRead)
+                {
+                    var x = reader.ReadInt32();
+                    var y = reader.ReadInt32();
+                    AliveCells.Add(new Point(x, y));
+                }
+
+                reader.Close();
+                stream.Close();
+            }
+        }
     }
 }
